39c1851061d356867444197ba5d70e323bfa9bf0
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-03-26 00:27:08 +0600

    fixed some things

--- authservice/views.py
+++ authservice/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from .forms import RegisterForm, LoginForm;
 from django.contrib.auth import logout;
 from django.views.generic import CreateView, TemplateView;
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.contrib import messages
 
