aabe0a63ba1e65f61a6ec557c2dbdb6d9efbafca
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-03-26 07:08:45 +0600

    homepage done

--- brand/models.py
+++ brand/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class BrandModel(models.Model) :
     name = models.CharField(max_length = 25);
 
-    def __str__(self) :
-        return self.name;
+    # def __str__(self) :
+    #     return self.name;
--- carmart/views.py
+++ carmart/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from brand.models import BrandModel
+from car.models import CarModel
 
 # Create your views here.
 def index(request) :
-    return render(request, 'index.html')
+    brands = BrandModel.objects.all();
+    cars = CarModel.objects.all();
+    
+    return render(request, 'index.html', {'brands' : brands, 'cars' : cars, 'total': len(cars) })
